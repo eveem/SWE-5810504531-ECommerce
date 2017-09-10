e300ffc2bb4bd119491c316a5c402d94c73d275a
eveem <monpriya.tammavong@gmail.com>
2017-09-10 14:21:13 +0700

    Send email complete

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import contactForm
 
 # Create your views here.
@@ -7,7 +8,13 @@ def contact(request):
 	form = contactForm(request.POST or None)
 
 	if form.is_valid():
-		print (form.cleaned_data['email'])
+		name = form.cleaned_data['name']
+		comment = form.cleaned_data['comment']
+		subject = 'Message from MYSITE.com'
+		message = '%s %s' %(comment, name)
+		emailFrom = form.cleaned_data['email']
+		emailTo = [settings.EMAIL_HOST_USER]
+		send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
 	context = locals()
 	template = 'contact.html'
 	return render(request, template, context)
--- tryTen/settings.py
+++ tryTen/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'eveemdjango@gmail.com'
+EMAIL_HOST_PASSWORD = '12345678eve'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
