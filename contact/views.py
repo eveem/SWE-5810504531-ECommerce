58eda1b3225e1255ee8b469d5d3b99cc2069b15e
eveem <monpriya.tammavong@gmail.com>
2017-09-10 15:07:26 +0700

    Contact page complete

--- contact/views.py
+++ contact/views.py
@@ -5,7 +5,9 @@ from .forms import contactForm
 
 # Create your views here.
 def contact(request):
+	title = 'Contact'
 	form = contactForm(request.POST or None)
+	confirm_message = None
 
 	if form.is_valid():
 		name = form.cleaned_data['name']
@@ -15,6 +17,10 @@ def contact(request):
 		emailFrom = form.cleaned_data['email']
 		emailTo = [settings.EMAIL_HOST_USER]
 		send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
-	context = locals()
-	template = 'contact.html'
+		title = "Thank!"
+		confirm_message = "Thanks for the message. We will get right back to you."
+		form = None
+	
+	context = {'title': title, 'form': form, 'confirm_message': confirm_message}
+	template = 'contact.html' 
 	return render(request, template, context)
