cb6ac1262112f4899c6d4d924d711cfbc1bc6dc2
eveem <monpriya.tammavong@gmail.com>
2017-09-10 16:56:42 +0700

    Add allauth

--- tryTen/urls.py
+++ tryTen/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from profiles import views as profiles_views
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^$', profiles_views.home, name='home'),
     url(r'^about/$', profiles_views.about, name='about'),
     url(r'^contact/$', contact_views.contact, name='contact'),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
